02ba843c7e6fc0d0d40f5a356b5592071d04cf7b
Brent Atchison <atchisonbrent@gmail.com>
2026-09-08 10:12:15 -0500

    perf(ui): reuse unchanged live activity tool rows

--- tests/test_issue5367_transparent_live_row_reconcile.py
+++ tests/test_issue5367_transparent_live_row_reconcile.py
@@ -313,6 +313,9 @@ eval(extractFunc('_refreshTransparentFadeProseRow'));
 eval(extractFunc('_refreshTransparentLiveRow'));
 eval(extractFunc('_restoreLiveAnchorScrollSnapshotAfterRebuild'));
 eval(extractFunc('_restoreLiveAnchorScrollSnapshotAfterRebuild'));
+document.documentElement = document.documentElement || Object.create(null);
+eval(extractFunc('_anchorSceneToolRenderSignature'));
+eval(extractFunc('_anchorSceneDataRowKey'));
 eval(extractFunc('_renderLiveAnchorActivitySceneTransparent'));
 
 const firstScene = {{
@@ -1268,6 +1271,9 @@ eval(extractFunc('_rehydrateTransparentLiveRow'));
 eval(extractFunc('_refreshTransparentThinkingLiveRow'));
 eval(extractFunc('_refreshTransparentLiveRow'));
 eval(extractFunc('_restoreLiveAnchorScrollSnapshotAfterRebuild'));
+document.documentElement = document.documentElement || Object.create(null);
+eval(extractFunc('_anchorSceneToolRenderSignature'));
+eval(extractFunc('_anchorSceneDataRowKey'));
 eval(extractFunc('_renderLiveAnchorActivitySceneTransparent'));
 
 const firstScene = { version:'activity_scene_v1', activity_rows:[{ row_id:'row-tool', role:'tool', source_event_type:'tool_delta', toolName:'old_tool' }] };
@@ -1606,6 +1612,9 @@ eval(extractFunc('_rehydrateTransparentLiveRow'));
 eval(extractFunc('_refreshTransparentThinkingLiveRow'));
 eval(extractFunc('_refreshTransparentLiveRow'));
 eval(extractFunc('_restoreLiveAnchorScrollSnapshotAfterRebuild'));
+document.documentElement = document.documentElement || Object.create(null);
+eval(extractFunc('_anchorSceneToolRenderSignature'));
+eval(extractFunc('_anchorSceneDataRowKey'));
 eval(extractFunc('_renderLiveAnchorActivitySceneTransparent'));
 global._copyEventToClipboard = (row) => {{
   const tc = row && row._tcData || {{}};
